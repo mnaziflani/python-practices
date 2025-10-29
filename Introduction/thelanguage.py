b5657667e03e964c2f290b8ed70bb8e144c4c14e
Nazif <mnaziflani@gmail.com>
2025-10-29 09:59:59 +0800

    Initialise Repo

---
+++ Introduction/thelanguage.py
@@ -0,0 +1,12 @@
+"""
+Python is one of the world's easiest and most popular programming languages.
+
+Welcome to your first Python program! The code is already written for you.
+
+What to do:
+Look at the code: print("Hello Python!")
+Press the "Run Code" button to execute it
+You should see "Hello Python!" appear in the output
+"""
+
+print("Hello world")

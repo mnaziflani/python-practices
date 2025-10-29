fb2dcdc6e116beeb4a143b6ff283d5893aeff2e5
Nazif <mnaziflani@gmail.com>
2025-10-29 10:29:17 +0800

    Introduction Package Completed

---
+++ Introduction/comments.py
@@ -0,0 +1,13 @@
+# This is a comment
+print("Hello!")  # This prints "Hello!"
+
+# You can use comments to explain your code
+print("Python is fun!")  # This will show on the screen
+
+"""
+This is a multi-line comment.
+You can write multiple lines
+of explanation here.
+"""
+
+print("Welcome!")
---
+++ Introduction/helloworld.py
@@ -0,0 +1,6 @@
+"""
+The "Hello World!" is a simple program that outputs Hello World! to the screen.
+In Python, we use print() to show words on the screen. The words go inside quotation marks.
+"""
+
+print("Hello world")

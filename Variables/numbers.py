cc5af2c408b51147a3aba6b72ceb57a858f88600
Nazif <mnaziflani@gmail.com>
2025-10-30 17:14:14 +0800

    Extending Variable Content

---
+++ Variables/numbers.py
@@ -0,0 +1,27 @@
+"""
+Variables are containers that hold data values. They are used to store, manipulate, and display information within a program.
+In short, a variable is like a memory unit that we can access by typing the name of the variable.
+
+Each variable has a unique name and a value that can be of different types. Python is capable of automatically detecting the variable type, which makes coding more efficient.
+To initialize a variable, we use the following format:
+
+variable_name = value
+Let's take a look at the different types of numbers:
+
+int - whole number, such as 1 or -2.
+float - real number, such as 1.32 or 0.98.
+
+For example:
+To initialize a variable of type int with the name a and the value 3:
+a = 3
+
+To initialize a variable of type float with the name b and the value 13.2:
+b = 13.2
+"""
+
+# Type your code below
+var = 20
+store_number = 100.5
+# Don't change the line below
+print(f'var = {var}')
+print(store_number)
---
+++ Variables/string.py
@@ -0,0 +1,15 @@
+"""
+A char is a single character (for example: 1, 6, %, b, p, ., T, etc.)
+The str (string) type is a special type that consists of multiple chars.
+To initialize a string value in a variable, enclose it within single or double quotation marks:
+
+s1 = 'This is a string'
+s2 = "This is also a string"
+In the above example, two string variables are initialized, named s1 and s2.
+"""
+
+# Type your code below
+name = """Alex"""
+
+# Don't change the line below
+print(f'name = {name}')

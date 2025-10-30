cf4f30d78a2c9cff0bf9af7378940f41d7aae0dd
Nazif <mnaziflani@gmail.com>
2025-10-30 17:18:14 +0800

    Boolean Content

---
+++ Variables/boolean.py
@@ -0,0 +1,18 @@
+"""
+A bool (Boolean) type has only 2 possible values: True or False.
+Here is an example of assigning a bool value to a variable:
+
+variable_true = True
+variable_false = False
+
+In the above, two variables named variable_true and variable_false are initialized, with the values True and False respectively.
+Booleans are the building blocks for creating logic in the programs we write. We have a whole chapter about logic and conditions.
+"""
+
+# Type your code below
+boolean = True
+
+# Don't change the line below
+trueorfalse = 2 > 5
+print(f'boolean = {boolean}')
+print(f'trueorfalse = {trueorfalse}')

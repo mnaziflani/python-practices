a26bc6f455167efdf34369d5ef7825203ed61f5b
Nazif <mnaziflani@gmail.com>
2025-11-01 23:38:42 +0800

    Added Arithmetic Shortcut

---
+++ [03] Operators/[3] Arithmetic Commands.py
@@ -0,0 +1,27 @@
+"""
+Python created a cool shortcut for self-arithmetic operations.
+
+For example, instead of writing:
+
+a = 5
+a = a + 3 # a holds 8
+We can simplify it by writing +=:
+
+a = 5
+a += 3 # a holds 8
+The += operator adds the value 3 to a.
+
+This operation is valid for all arithmetic operations:
+
+Operator	Shortcut
++	+=
+-	-=
+*	*=
+/	/=
+%	%=
+
+"""
+
+a = 50
+a += 5
+print(a)
